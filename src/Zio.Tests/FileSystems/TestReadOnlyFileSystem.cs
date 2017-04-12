1d8102baae92244c0221cd59c6890916b7286897
Alexandre Mutel <alexandre_mutel@live.com>
2017-04-12 23:16:16 +0200

    Add tests for ReadOnlyFileSystem

--- src/Zio.Tests/FileSystems/TestReadOnlyFileSystem.cs
+++ src/Zio.Tests/FileSystems/TestReadOnlyFileSystem.cs
@@ -3,6 +3,7 @@
 // See the license.txt file in the project root for more information.
 
 using System;
+using System.IO;
 using Xunit;
 using Zio.FileSystems;
 
@@ -11,14 +12,27 @@ namespace Zio.Tests.FileSystems
     public class TestReadOnlyFileSystem : TestFileSystemBase
     {
         [Fact]
-        public void TestDirectory()
+        public void TestApi()
         {
             var fs = new PhysicalFileSystem();
             var rofs = new ReadOnlyFileSystem(fs);
 
-            Assert.Throws<InvalidOperationException>(() => rofs.CreateDirectory("test"));
-        }
+            Assert.True(rofs.DirectoryExists("/"));
 
-        
+            Assert.Throws<InvalidOperationException>(() => rofs.CreateDirectory("/test"));
+            Assert.Throws<InvalidOperationException>(() => rofs.DeleteDirectory("/test", true));
+            Assert.Throws<InvalidOperationException>(() => rofs.MoveDirectory("/drive", "/drive2"));
+
+            Assert.Throws<InvalidOperationException>(() => rofs.CreateFile("/toto.txt"));
+            Assert.Throws<InvalidOperationException>(() => rofs.CopyFile("/toto.txt", "/dest.txt", true));
+            Assert.Throws<InvalidOperationException>(() => rofs.MoveFile("/drive", "/drive2"));
+            Assert.Throws<InvalidOperationException>(() => rofs.DeleteFile("/toto.txt"));
+            Assert.Throws<InvalidOperationException>(() => rofs.OpenFile("/toto.txt", FileMode.Create, FileAccess.ReadWrite));
+
+            Assert.Throws<InvalidOperationException>(() => rofs.SetAttributes("/toto.txt", FileAttributes.ReadOnly));
+            Assert.Throws<InvalidOperationException>(() => rofs.SetCreationTime("/toto.txt", DateTime.Now));
+            Assert.Throws<InvalidOperationException>(() => rofs.SetLastAccessTime("/toto.txt", DateTime.Now));
+            Assert.Throws<InvalidOperationException>(() => rofs.SetLastWriteTime("/toto.txt", DateTime.Now));
+        }
     }
 }
