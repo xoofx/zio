b932bd67983a139def7886ecb74c957ce018a50a
Alexandre Mutel <alexandre_mutel@live.com>
2017-04-12 23:34:20 +0200

    Add tests for SubFileSystem

---
+++ src/Zio.Tests/FileSystems/TestSubFileSystem.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license. 
+// See the license.txt file in the project root for more information.
+
+using System.IO;
+using System.Linq;
+using Xunit;
+using Zio.FileSystems;
+
+namespace Zio.Tests.FileSystems
+{
+    public class TestSubFileSystem : TestFileSystemBase
+    {
+        [Fact]
+        public void TestBasic()
+        {
+            var fs = new PhysicalFileSystem();
+            var path = fs.ConvertFromSystem(SystemPath);
+
+            // Create a filesystem / on the current folder of this assembly
+            var subfs = new SubFileSystem(fs, path);
+
+            // This test is basically testing the two methods (ConvertPathToDelegate and ConvertPathFromDelegate) in SubFileSystem
+
+            var files = subfs.EnumeratePaths("/").Select(info => info.GetName()).ToList();
+            var expectedFiles = fs.EnumeratePaths(path).Select(info => info.GetName()).ToList();
+            Assert.True(files.Count > 0);
+            Assert.Equal(expectedFiles, files);
+
+            // Check that SubFileSystem is actually checking that the directory exists in the delegate filesystem
+            Assert.Throws<DirectoryNotFoundException>(() => new SubFileSystem(fs, path / "does_not_exist"));
+
+            // TODO: We could add another test just to make sure that files can be created...etc. But the test above should already cover the code provided in SubFileSystem
+        }
+    }
+}
--- src/Zio/FileSystems/SubFileSystem.cs
+++ src/Zio/FileSystems/SubFileSystem.cs
@@ -2,32 +2,33 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 using System;
+using System.IO;
 
 namespace Zio.FileSystems
 {
     /// <summary>
-    /// Provides a secure view on a sub folder of another <see cref="IFileSystem"/>
+    /// Provides a secure view on a sub folder of another delegate <see cref="IFileSystem"/>
     /// </summary>
     public class SubFileSystem : DelegateFileSystem
     {
-        public SubFileSystem(IFileSystem fileSystem, string subSystemPath) : base(fileSystem)
-        {
-            SubPath = fileSystem.ConvertFromSystem(subSystemPath);
-            if (!fileSystem.DirectoryExists(SubPath))
-            {
-                throw new ArgumentException($"The directory `{SubPath}` does not exist in the delegated FileSystem", nameof(subSystemPath));
-            }
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubFileSystem"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system to create a view from.</param>
+        /// <param name="subPath">The sub path view to create filesystem.</param>
+        /// <exception cref="DirectoryNotFoundException">If the directory subPath does not exist in the delegate FileSystem</exception>
         public SubFileSystem(IFileSystem fileSystem, PathInfo subPath) : base(fileSystem)
         {
             SubPath = subPath.AssertAbsolute(nameof(subPath));
             if (!fileSystem.DirectoryExists(SubPath))
             {
-                throw new ArgumentException($"The directory `{SubPath}` does not exist in the delegated FileSystem", nameof(subPath));
+                throw new DirectoryNotFoundException($"The directory `{SubPath}` does not exist in the delegated FileSystem");
             }
         }
 
+        /// <summary>
+        /// Gets the sub path relative to the delegate <see cref="DelegateFileSystem.NextFileSystem"/>
+        /// </summary>
         public PathInfo SubPath { get; }
 
         protected override PathInfo ConvertPathToDelegate(PathInfo path)
@@ -41,7 +42,9 @@ namespace Zio.FileSystems
             var fullPath = path.FullName;
             if (!fullPath.StartsWith(SubPath.FullName) || fullPath.Length <= SubPath.FullName.Length || fullPath[SubPath.FullName.Length] != PathInfo.DirectorySeparator)
             {
-                throw new InvalidOperationException($"The path `{path}` is not rooted to `{SubPath}`");
+                // More a safe guard, as it should never happen, but if a delegate filesystem doesn't respect its root path
+                // we are throwing an exception here
+                throw new InvalidOperationException($"The path `{path}` returned by the delegate filesystem is not rooted to the subpath `{SubPath}`");
             }
 
             return new PathInfo(fullPath.Substring(SubPath.FullName.Length), true);
