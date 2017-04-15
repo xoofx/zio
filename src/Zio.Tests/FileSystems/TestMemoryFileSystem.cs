8aa0080b531231010f1977641e609e6fdd68b756
Alexandre Mutel <alexandre_mutel@live.com>
2017-04-15 12:04:54 +0200

    Add tests for MemoryFileStream (wip)

---
+++ src/Zio.Tests/FileSystems/TestMemoryFileSystem.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license. 
+// See the license.txt file in the project root for more information.
+
+using Xunit;
+using Zio.FileSystems;
+
+namespace Zio.Tests.FileSystems
+{
+    public class TestMemoryFileSystem : TestFileSystemBase
+    {
+        [Fact]
+        public void TestDirectory()
+        {
+            var fs = new MemoryFileSystem();
+
+            fs.CreateDirectory("/test");
+
+            Assert.True(fs.DirectoryExists("/test"));
+
+            fs.CreateDirectory("/test/test2");
+            Assert.True(fs.DirectoryExists("/test/test2"));
+
+            fs.DeleteDirectory("/test", true);
+            Assert.False(fs.DirectoryExists("/test"));
+        }
+    }
+}
