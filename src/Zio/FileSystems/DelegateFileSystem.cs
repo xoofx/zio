09c1c959dfa41c77a0e9a076fb7aa32a5e902c18
Alexandre Mutel <alexandre_mutel@live.com>
2017-04-27 21:54:32 +0200

    Add checks for a null delegate filesystem

--- src/Zio/FileSystems/DelegateFileSystem.cs
+++ src/Zio/FileSystems/DelegateFileSystem.cs
@@ -16,16 +16,30 @@ namespace Zio.FileSystems
         /// Initializes a new instance of the <see cref="DelegateFileSystem"/> class.
         /// </summary>
         /// <param name="fileSystem">The delegated file system.</param>
-        /// <exception cref="System.ArgumentNullException">fileSystem</exception>
         protected DelegateFileSystem(IFileSystem fileSystem)
         {
             NextFileSystem = fileSystem;
         }
 
         /// <summary>
-        /// Gets the next delegated file system.
+        /// Gets the next delegated file system (may be null).
         /// </summary>
         protected IFileSystem NextFileSystem { get; }
+
+        /// <summary>
+        /// Gets the next delegated file system.
+        /// </summary>
+        protected IFileSystem NextFileSystemSafe
+        {
+            get
+            {
+                if (NextFileSystem == null)
+                {
+                    throw new InvalidOperationException("The delegate filesystem for this instance is null");
+                }
+                return NextFileSystem;
+            }
+        }
 
         // ----------------------------------------------
         // Directory API
@@ -34,25 +48,25 @@ namespace Zio.FileSystems
         /// <inheritdoc />
         protected override void CreateDirectoryImpl(UPath path)
         {
-            NextFileSystem.CreateDirectory(ConvertPathToDelegate(path));
+            NextFileSystemSafe.CreateDirectory(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override bool DirectoryExistsImpl(UPath path)
         {
-            return NextFileSystem.DirectoryExists(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.DirectoryExists(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override void MoveDirectoryImpl(UPath srcPath, UPath destPath)
         {
-            NextFileSystem.MoveDirectory(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath));
+            NextFileSystemSafe.MoveDirectory(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath));
         }
 
         /// <inheritdoc />
         protected override void DeleteDirectoryImpl(UPath path, bool isRecursive)
         {
-            NextFileSystem.DeleteDirectory(ConvertPathToDelegate(path), isRecursive);
+            NextFileSystemSafe.DeleteDirectory(ConvertPathToDelegate(path), isRecursive);
         }
 
         // ----------------------------------------------
@@ -62,44 +76,44 @@ namespace Zio.FileSystems
         /// <inheritdoc />
         protected override void CopyFileImpl(UPath srcPath, UPath destPath, bool overwrite)
         {
-            NextFileSystem.CopyFile(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath), overwrite);
+            NextFileSystemSafe.CopyFile(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath), overwrite);
         }
 
         /// <inheritdoc />
         protected override void ReplaceFileImpl(UPath srcPath, UPath destPath, UPath destBackupPath,
             bool ignoreMetadataErrors)
         {
-            NextFileSystem.ReplaceFile(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath), destBackupPath.IsNull ? destBackupPath : ConvertPathToDelegate(destBackupPath), ignoreMetadataErrors);
+            NextFileSystemSafe.ReplaceFile(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath), destBackupPath.IsNull ? destBackupPath : ConvertPathToDelegate(destBackupPath), ignoreMetadataErrors);
         }
 
         /// <inheritdoc />
         protected override long GetFileLengthImpl(UPath path)
         {
-            return NextFileSystem.GetFileLength(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.GetFileLength(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override bool FileExistsImpl(UPath path)
         {
-            return NextFileSystem.FileExists(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.FileExists(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override void MoveFileImpl(UPath srcPath, UPath destPath)
         {
-            NextFileSystem.MoveFile(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath));
+            NextFileSystemSafe.MoveFile(ConvertPathToDelegate(srcPath), ConvertPathToDelegate(destPath));
         }
 
         /// <inheritdoc />
         protected override void DeleteFileImpl(UPath path)
         {
-            NextFileSystem.DeleteFile(ConvertPathToDelegate(path));
+            NextFileSystemSafe.DeleteFile(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override Stream OpenFileImpl(UPath path, FileMode mode, FileAccess access, FileShare share = FileShare.None)
         {
-            return NextFileSystem.OpenFile(ConvertPathToDelegate(path), mode, access, share);
+            return NextFileSystemSafe.OpenFile(ConvertPathToDelegate(path), mode, access, share);
         }
 
         // ----------------------------------------------
@@ -109,49 +123,49 @@ namespace Zio.FileSystems
         /// <inheritdoc />
         protected override FileAttributes GetAttributesImpl(UPath path)
         {
-            return NextFileSystem.GetAttributes(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.GetAttributes(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override void SetAttributesImpl(UPath path, FileAttributes attributes)
         {
-            NextFileSystem.SetAttributes(ConvertPathToDelegate(path), attributes);
+            NextFileSystemSafe.SetAttributes(ConvertPathToDelegate(path), attributes);
         }
 
         /// <inheritdoc />
         protected override DateTime GetCreationTimeImpl(UPath path)
         {
-            return NextFileSystem.GetCreationTime(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.GetCreationTime(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override void SetCreationTimeImpl(UPath path, DateTime time)
         {
-            NextFileSystem.SetCreationTime(ConvertPathToDelegate(path), time);
+            NextFileSystemSafe.SetCreationTime(ConvertPathToDelegate(path), time);
         }
 
         /// <inheritdoc />
         protected override DateTime GetLastAccessTimeImpl(UPath path)
         {
-            return NextFileSystem.GetLastAccessTime(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.GetLastAccessTime(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override void SetLastAccessTimeImpl(UPath path, DateTime time)
         {
-            NextFileSystem.SetLastAccessTime(ConvertPathToDelegate(path), time);
+            NextFileSystemSafe.SetLastAccessTime(ConvertPathToDelegate(path), time);
         }
 
         /// <inheritdoc />
         protected override DateTime GetLastWriteTimeImpl(UPath path)
         {
-            return NextFileSystem.GetLastWriteTime(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.GetLastWriteTime(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override void SetLastWriteTimeImpl(UPath path, DateTime time)
         {
-            NextFileSystem.SetLastWriteTime(ConvertPathToDelegate(path), time);
+            NextFileSystemSafe.SetLastWriteTime(ConvertPathToDelegate(path), time);
         }
 
         // ----------------------------------------------
@@ -161,7 +175,7 @@ namespace Zio.FileSystems
         /// <inheritdoc />
         protected override IEnumerable<UPath> EnumeratePathsImpl(UPath path, string searchPattern, SearchOption searchOption, SearchTarget searchTarget)
         {
-            foreach (var subPath in NextFileSystem.EnumeratePaths(ConvertPathToDelegate(path), searchPattern, searchOption, searchTarget))
+            foreach (var subPath in NextFileSystemSafe.EnumeratePaths(ConvertPathToDelegate(path), searchPattern, searchOption, searchTarget))
             {
                 yield return ConvertPathFromDelegate(subPath);
             }
@@ -174,13 +188,13 @@ namespace Zio.FileSystems
         /// <inheritdoc />
         protected override string ConvertToSystemImpl(UPath path)
         {
-            return NextFileSystem.ConvertToSystem(ConvertPathToDelegate(path));
+            return NextFileSystemSafe.ConvertToSystem(ConvertPathToDelegate(path));
         }
 
         /// <inheritdoc />
         protected override UPath ConvertFromSystemImpl(string systemPath)
         {
-            return ConvertPathFromDelegate(NextFileSystem.ConvertFromSystem(systemPath));
+            return ConvertPathFromDelegate(NextFileSystemSafe.ConvertFromSystem(systemPath));
         }
 
         /// <summary>
