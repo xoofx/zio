dbfdc8d4080ee3e89ddbf3b59af1c65e3c151def
Alexandre Mutel <alexandre_mutel@live.com>
2017-04-28 22:25:12 +0200

    Add comments for UPath

--- src/Zio/UPath.cs
+++ src/Zio/UPath.cs
@@ -9,17 +9,26 @@ using System.Text;
 namespace Zio
 {
     /// <summary>
-    /// Uniform and secure path wrapper.
+    /// A uniform unix like path.
     /// </summary>
     /// <seealso cref="UPath" />
     public struct UPath : IEquatable<UPath>, IComparable<UPath>
     {
         [ThreadStatic] private static InternalHelper _internalHelperTls;
 
+        /// <summary>
+        /// An empty path.
+        /// </summary>
         public static readonly UPath Empty = new UPath(string.Empty, true);
 
+        /// <summary>
+        /// The root path `/`
+        /// </summary>
         public static readonly UPath Root = new UPath("/", true);
 
+        /// <summary>
+        /// The directory separator `/`
+        /// </summary>
         public const char DirectorySeparator = '/';
 
         private static InternalHelper InternalHelperTls => _internalHelperTls ?? (_internalHelperTls = new InternalHelper());
@@ -50,27 +59,68 @@ namespace Zio
             }
         }
 
+        /// <summary>
+        /// Gets the full name of this path (Note hat it may be null).
+        /// </summary>
+        /// <value>The full name of this path.</value>
         public string FullName { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether this path is null.
+        /// </summary>
+        /// <value><c>true</c> if this instance is null; otherwise, <c>false</c>.</value>
         public bool IsNull => FullName == null;
 
+        /// <summary>
+        /// Gets a value indicating whether this path is empty (<see cref="FullName"/> equals to the empty string)
+        /// </summary>
+        /// <value><c>true</c> if this instance is empty; otherwise, <c>false</c>.</value>
         public bool IsEmpty => FullName == string.Empty;
 
+        /// <summary>
+        /// Gets a value indicating whether this path is absolute by starting with a leading `/`.
+        /// </summary>
+        /// <value><c>true</c> if this path is absolute; otherwise, <c>false</c>.</value>
         public bool IsAbsolute => FullName?.StartsWith("/") ?? false;
 
+        /// <summary>
+        /// Gets a value indicating whether this path is relative by **not** starting with a leading `/`.
+        /// </summary>
+        /// <value><c>true</c> if this instance is relative; otherwise, <c>false</c>.</value>
         public bool IsRelative => !IsAbsolute;
 
-
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.String"/> to <see cref="UPath"/>.
+        /// </summary>
+        /// <param name="path">The path as a string.</param>
+        /// <returns>The result of the conversion.</returns>
         public static implicit operator UPath(string path)
         {
             return new UPath(path);
         }
 
+        /// <summary>
+        /// Performs an explicit conversion from <see cref="UPath"/> to <see cref="System.String"/>.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>The result as a string of the conversion.</returns>
         public static explicit operator string(UPath path)
         {
             return path.FullName;
         }
 
+        /// <summary>
+        /// Combines two paths into a new path.
+        /// </summary>
+        /// <param name="path1">The first path to combine.</param>
+        /// <param name="path2">The second path to combine.</param>
+        /// <returns>The combined paths. If one of the specified paths is a zero-length string, this method returns the other path. If path2 contains an absolute path, this method returns path2.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// path1
+        /// or
+        /// path2
+        /// </exception>
+        /// <exception cref="System.ArgumentException">If an error occurs while trying to combine paths.</exception>
         public static UPath Combine(UPath path1, UPath path2)
         {
             if (path1.FullName == null)
@@ -107,45 +157,79 @@ namespace Zio
             }
             catch (ArgumentException ex)
             {
-                throw new InvalidOperationException($"Unable to combine path `{path1}` with `{path2}`", ex);
+                throw new ArgumentException($"Unable to combine path `{path1}` with `{path2}`", ex);
             }
         }
 
+        /// <summary>
+        /// Implements the / operator equivalent of <see cref="Combine"/>
+        /// </summary>
+        /// <param name="path1">The first path to combine.</param>
+        /// <param name="path2">The second path to combine.</param>
+        /// <returns>The combined paths. If one of the specified paths is a zero-length string, this method returns the other path. If path2 contains an absolute path, this method returns path2.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// path1
+        /// or
+        /// path2
+        /// </exception>
+        /// <exception cref="System.ArgumentException">If an error occurs while trying to combine paths.</exception>
         public static UPath operator /(UPath path1, UPath path2)
         {
             return Combine(path1, path2);
         }
 
+        /// <inheritdoc />
         public bool Equals(UPath other)
         {
             return string.Equals(FullName, other.FullName);
         }
 
+        /// <inheritdoc />
         public override bool Equals(object obj)
         {
             return obj is UPath && Equals((UPath) obj);
         }
 
+        /// <inheritdoc />
         public override int GetHashCode()
         {
             return FullName?.GetHashCode() ?? 0;
         }
 
+        /// <summary>
+        /// Implements the == operator.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>The result of the operator.</returns>
         public static bool operator ==(UPath left, UPath right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Implements the != operator.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        /// <returns>The result of the operator.</returns>
         public static bool operator !=(UPath left, UPath right)
         {
             return !left.Equals(right);
         }
 
+        /// <inheritdoc />
         public override string ToString()
         {
             return FullName;
         }
 
+        /// <summary>
+        /// Tries to parse the specified string into a <see cref="UPath"/>
+        /// </summary>
+        /// <param name="path">The path as a string.</param>
+        /// <param name="pathInfo">The path parsed if successfull.</param>
+        /// <returns><c>true</c> if path was parsed successfully, <c>false</c> otherwise.</returns>
         public static bool TryParse(string path, out UPath pathInfo)
         {
             string errorMessage;
