b65f2770711d2cbd290c5d17a08bfe91a7d158b5
Alexandre Mutel <alexandre_mutel@live.com>
2017-05-01 11:27:24 +0200

    Add missing tests for FileSystemEntry

--- src/Zio.Tests/FileSystems/TestFileSystemEntries.cs
+++ src/Zio.Tests/FileSystems/TestFileSystemEntries.cs
@@ -2,6 +2,9 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
+using System;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Zio.Tests.FileSystems
@@ -55,6 +58,37 @@ namespace Zio.Tests.FileSystems
             }
 
             Assert.Equal(new byte[] {1, 2, 3}, memfs.ReadAllBytes("/a/yoyo.txt"));
+
+            Assert.Throws<FileNotFoundException>(() => memfs.GetFileSystemEntry("/wow.txt"));
+
+            var file3 = memfs.GetFileEntry("/a/b.txt");
+            Assert.True(file3.Exists);
+
+            Assert.Null(memfs.TryGetFileSystemEntry("/invalid_file"));
+            Assert.IsType<FileEntry>(memfs.TryGetFileSystemEntry("/a/b.txt"));
+            Assert.IsType<DirectoryEntry>(memfs.TryGetFileSystemEntry("/a"));
+
+            Assert.Throws<FileNotFoundException>(() => memfs.GetFileEntry("/invalid"));
+            Assert.Throws<DirectoryNotFoundException>(() => memfs.GetDirectoryEntry("/invalid"));
+
+
+            var mydir = new DirectoryEntry(memfs, "/yoyo");
+
+            Assert.Throws<ArgumentException>(() => mydir.CreateSubdirectory("/sub"));
+
+            var subFolder = mydir.CreateSubdirectory("sub");
+            Assert.True(subFolder.Exists);
+
+            Assert.Equal(0, mydir.EnumerateFiles().ToList().Count);
+
+            var subDirs = mydir.EnumerateDirectories().ToList();
+            Assert.Equal(1, subDirs.Count);
+            Assert.Equal("/yoyo/sub", subDirs[0].FullName);
+
+            mydir.Delete();
+
+            Assert.False(mydir.Exists);
+            Assert.False(subFolder.Exists);
         }
     }
 }
