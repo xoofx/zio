49861f8ab278a9f6a782311204b3520ed364d3cc
Alexandre Mutel <alexandre_mutel@live.com>
2017-05-01 11:26:57 +0200

    Add test for FileSystem.Dispose(0

--- src/Zio.Tests/FileSystems/TestMemoryFileSystem.cs
+++ src/Zio.Tests/FileSystems/TestMemoryFileSystem.cs
@@ -2,7 +2,9 @@
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
+using System;
 using Xunit;
+using Zio.FileSystems;
 
 namespace Zio.Tests.FileSystems
 {
@@ -13,6 +15,15 @@ namespace Zio.Tests.FileSystems
         {
             var fs = GetCommonMemoryFileSystem();
             AssertCommonRead(fs);
+        }
+
+        [Fact]
+        public void TestDispose()
+        {
+            var memfs = new MemoryFileSystem();
+
+            memfs.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => memfs.DirectoryExists("/"));
         }
     }
 }
